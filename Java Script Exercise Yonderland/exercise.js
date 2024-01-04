6a4f947b469d996a98467b8700a277629b024012
Mata <vitorhmata@gmail.com>
2024-01-04 11:13:02 +0000

    first commit

--- Java Script Exercise Yonderland/exercise.js
+++ Java Script Exercise Yonderland/exercise.js
@@ -2,22 +2,24 @@
 function findLargestNumber(arr) {
     if (arr.length === 0) {
         console.log("The array is empty");
-        return;
+        return; //for the case of an empty array
     }
 
-    let largest = arr[0];
+    let largest = arr[0]; //assuming the first element is the largest
 
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] > largest) {
-            largest = arr[i];
+            largest = arr[i]; //testing element by element
         }
     }
 
     console.log("The largest number is: " + largest);
 }
 
+//First Test
 let numbers = [2, 43, 5, 76, 7];
 findLargestNumber(numbers);
 
+//Second Test
 let empty = [];
 findLargestNumber(empty);
